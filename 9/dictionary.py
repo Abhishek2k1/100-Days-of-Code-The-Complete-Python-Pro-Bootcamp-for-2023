6a67111cdc9b394f84d7ba331f6defa89377f5af
ABHISHEK PAUL <abhishek898paul@gmail.com>
2023-09-10 12:46:54 +0530

    day 9

---
+++ 9/dictionary.py
@@ -0,0 +1,8 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+# Retreving items from a dictionary
+
+print(programming_dictionary["Bug"])
